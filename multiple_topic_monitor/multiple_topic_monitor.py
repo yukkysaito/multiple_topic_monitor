78bd3e19f5d02b901ed88877068d561511574695
Yukihito Saito <yukky.saito@gmail.com>
2024-04-21 03:47:41 +0900

    bug fix
    Signed-off-by: Yukihito Saito <yukky.saito@gmail.com>

--- multiple_topic_monitor/multiple_topic_monitor.py
+++ multiple_topic_monitor/multiple_topic_monitor.py
@@ -1,3 +1,34 @@
+# Copyright (c) 2008, Willow Garage, Inc.
+# SPDX-License-Identifier: BSD-3-Clause
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the copyright holder nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# Additional code written by Yukihiro Saito under Apache License, Version 2.0
+
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
@@ -23,7 +54,6 @@ class TopicMonitor(Node):
         self.window_size = window_size
         self.csv_mode = csv_mode
         self.topic_stats = {topic: {"times": deque(maxlen=window_size), "delays": deque(maxlen=window_size)} for topic in topics}
-
         self.first_row_printed = False
 
         for topic in topics:
@@ -38,13 +68,15 @@ class TopicMonitor(Node):
                 self.get_logger().info(f"Subscribed to {topic}")
             else:
                 self.get_logger().error(f"No message class found for topic {topic}")
+                self.topic_stats[topic]['header'] = False
+                self.get_logger().info(f"No header found for topic {topic}")
 
         self.create_timer(1.0, self.print_stats)
 
     def message_callback(self, msg, topic):
         now = self.get_clock().now()
-        self.topic_stats[topic]["times"].append((now.nanoseconds, now.to_msg().sec, now.to_msg().nanosec))
-        
+        self.topic_stats[topic]["times"].append(now.nanoseconds)
+
         if hasattr(msg, 'header') and hasattr(msg.header, 'stamp'):
             msg_time = msg.header.stamp
             if msg_time:
@@ -56,37 +88,54 @@ class TopicMonitor(Node):
 
     def print_stats(self):
         now = datetime.now()
-        if self.csv_mode and not self.first_row_printed:
-            # Print header row
-            header = "timestamp, " + ", ".join([f"{topic} hz" for topic in self.topic_stats.keys()] +
-                                               [f"{topic} delay[ms]" for topic in self.topic_stats.keys()])
-            print(header)
-            self.first_row_printed = True
-
-        row_values = []
-        for topic, stats in self.topic_stats.items():
-            times = stats["times"]
-            if len(times) > 1:
-                dt = times[-1][0] - times[0][0]
-                count = len(times) - 1
-                hz = count / (dt / 1e9) if dt else 0
-                row_values.append(f"{hz:.2f}")
-            else:
-                row_values.append("NA")
-
-        for topic, stats in self.topic_stats.items():
-            delays = stats["delays"]
-            if len(delays):
-                avg_delay = sum(delays) / len(delays) / 1e6  # Convert nanoseconds to milliseconds
-                row_values.append(f"{avg_delay:.2f}")
-            else:
-                row_values.append("NA")
-
-        timestamp = now.strftime("%Y-%m-%d %H:%M:%S.%f")
         if self.csv_mode:
+            if not self.first_row_printed:
+                # Print header row for CSV
+                header = "timestamp," + ", ".join([f"{topic} Hz" for topic in self.topic_stats.keys()] +
+                                                  [f"{topic} delay[ms]" for topic in self.topic_stats.keys()])
+                print(header)
+                self.first_row_printed = True
+
+            row_values = []
+            for topic, stats in self.topic_stats.items():
+                times = stats["times"]
+                if len(times) > 1:
+                    dt = times[-1] - times[0]
+                    count = len(times) - 1
+                    hz = count / (dt / 1e9) if dt else 0
+                    row_values.append(f"{hz:.2f}")
+                else:
+                    row_values.append("NA")
+
+            for topic, stats in self.topic_stats.items():
+                delays = stats["delays"]
+                if len(delays):
+                    avg_delay = sum(delays) / len(delays) / 1e6  # Convert nanoseconds to milliseconds
+                    row_values.append(f"{avg_delay:.2f}")
+                else:
+                    row_values.append("NA")
+
+            timestamp = now.strftime("%Y-%m-%d %H:%M:%S.%f")
             print(f"{timestamp}, {', '.join(row_values)}")
         else:
-            self.get_logger().info(f"{timestamp}, {', '.join(row_values)}")
+            divider = "-" * 10
+            self.get_logger().info(f"{divider}{now.strftime('%Y-%m-%d %H:%M:%S.%f')}{divider}")
+
+            for topic, stats in self.topic_stats.items():
+                times = stats["times"]
+                if len(times) > 1:
+                    dt = times[-1] - times[0]
+                    count = len(times) - 1
+                    hz = count / (dt / 1e9) if dt else 0
+                    self.get_logger().info(f"{topic} Hz: {hz:.2f}")
+
+                delays = stats["delays"]
+                if len(delays):
+                    avg_delay = sum(delays) / len(delays) / 1e9
+                    min_delay = min(delays) / 1e9
+                    max_delay = max(delays) / 1e9
+                    std_dev = math.sqrt(sum((x - avg_delay * 1e9) ** 2 for x in delays) / len(delays)) / 1e9
+                    self.get_logger().info(f"{topic} Delay (s): Avg: {avg_delay:.3f}, Min: {min_delay:.3f}, Max: {max_delay:.3f}, Std Dev: {std_dev:.5f}")
 
 def main(args=None):
     parser = ArgumentParser(description="Measure the frequency and delay of topics")
